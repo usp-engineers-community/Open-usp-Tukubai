25c353bd5c6458c16cb326f2937f6aa7e1df3295
Ryuichi UEDA <ryuichiueda@gmail.com>
2012-08-16 22:04:20 +0900

    addition of retu (Haskell)

---
+++ COMMANDS.HS/retu.hs
@@ -0,0 +1,86 @@
+import qualified Data.ByteString.Lazy.Char8 as BS
+import System.Environment
+import System.IO
+
+{--
+retu（Open usp Tukubai）
+
+designed by Nobuaki Tounaka
+written by Ryuichi Ueda
+
+The MIT License
+
+Copyright (C) 2012 Universal Shell Programming Laboratory
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+--}
+
+showUsage :: IO ()
+showUsage = do hPutStr stderr
+		("Usage    : retu [-f] <file>\n" ++ 
+		"Thu Aug 16 21:56:10 JST 2012\n" ++
+		"Open usp Tukubai (LINUX+FREEBSD), Haskell ver.\n")
+
+main :: IO ()
+main = do
+	args <- getArgs
+	case args of
+		[] -> printCount
+		["-"] -> printCount
+		("-f":as) -> fmode as
+		["-h"] -> showUsage
+		["--help"] -> showUsage
+		_ -> mmode args
+	
+printCount :: IO ()
+printCount = BS.getContents >>= putStr . unlines . toStr . countFile
+
+printCountM :: String -> IO ()
+printCountM arg = BS.readFile arg >>= putStr . unlines . toStr . countFile
+	
+printCountF :: String -> IO()
+printCountF arg = do
+			bs <- BS.readFile arg 
+			putStr $ unlines $ countFileF arg bs
+	
+fmode :: [String] -> IO ()
+fmode [] = putStr ""
+fmode (arg:args) = do printCountF arg ; fmode args
+
+countFileF :: String -> BS.ByteString -> [String]
+countFileF arg bs = [ arg ++ " " ++ show(x) | x <- (countFile bs) ]
+
+mmode :: [String] -> IO ()
+mmode [] = putStr ""
+mmode (arg:args) = do printCountM arg; mmode args
+	
+countFile :: BS.ByteString -> [Int]
+countFile bs = unq ( countLine( BS.lines bs ) )
+
+countLine :: [BS.ByteString] -> [Int]
+countLine [] = []
+countLine (ln:lns) = length ( BS.split ' ' ln ) : countLine lns
+
+unq :: [Int] -> [Int]
+unq [n] = [n]
+unq (n:ns) = if n == head ns then unq (ns) else n : unq (ns)
+
+toStr :: [Int] -> [String]
+toStr [] = []
+toStr (n:ns) = show n : toStr(ns)
