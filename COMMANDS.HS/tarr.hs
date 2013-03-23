7598b6d64349ba6ec96291bc49a38a5a17b992f4
Ryuichi UEDA <ryuichiueda@gmail.com>
2013-03-23 21:58:21 +0900

    release of Haskell tarr

--- COMMANDS.HS/tarr.hs
+++ COMMANDS.HS/tarr.hs
@@ -39,7 +39,7 @@ THE SOFTWARE.
 showUsage :: IO ()
 showUsage = do hPutStr stderr
 		("Usage    : tarr [num=<n>] [-<m>] <file>\n" ++ 
-		"Fri Mar 22 22:07:15 JST 2013\n" ++
+		"Sat Mar 23 21:53:43 JST 2013\n" ++
 		"Open usp Tukubai (LINUX+FREEBSD), Haskell ver.\n")
 
 main :: IO ()
@@ -50,24 +50,54 @@ main = do
 		["--help"] -> showUsage
 		[]         -> do foldAll
 		["-"]      -> do foldAll
-		_          -> do print ( parseArgs args ) 
-
-parseArgs :: [String] -> (Int,Int)
-parseArgs as = (getNum $ head0 $ filter findNum as, getM $ head0 $ filter findM as)
-	where
-		findNum as = "num=" `isPrefixOf` as
-		findM as = "-" `isPrefixOf` as
-		getNum a = read (drop 4 a)::Int
-		getM a = read (drop 1 a)::Int
-
-head0 :: [String] -> String
-head0 [] = "00000"
-head0 as = head as
+		_          -> do if (fileName args) == "-" 
+					then BS.getContents >>= mainProc (parseArgs args)
+					else BS.readFile (fileName args) >>= mainProc (parseArgs args)
 
 foldAll = BS.getContents >>= putBSLines . fold
 
 putBSLines :: BS.ByteString -> IO ()
 putBSLines = putStr . CBUS.decodeString . BS.unpack 
 
+mainProc :: (Int,Int) -> BS.ByteString -> IO()
+mainProc (num,m) cs = if num == 0 then putBSLines $ mFold m cs
+				  else putBSLines $ keyFold num m ( BS.lines cs )
+
+mFold :: Int -> BS.ByteString -> BS.ByteString
+mFold m cs = BS.unlines $ concat [ mSet m ( BS.words c ) | c <- BS.lines cs ]
+
+mSet :: Int -> [ BS.ByteString ] -> [ BS.ByteString ]
+mSet m [] = []
+mSet m ws = (BS.unwords $ fst s) : mSet m (snd s)
+		where s = splitAt m ws 
+
+keyFold :: Int -> Int -> [ BS.ByteString ] -> BS.ByteString
+keyFold num m cs = BS.unlines $ concat [ keyExpand num m ( BS.words c ) | c <- cs ]
+
 fold :: BS.ByteString -> BS.ByteString
-fold cs = BS.unlines (concat [ BS.words c | c <- BS.lines cs ] )
+fold cs = BS.unlines $ concat [ BS.words c | c <- BS.lines cs ]
+
+fileName :: [String] -> String
+fileName as = if "num=" `isPrefixOf` lst || "-" `isPrefixOf` lst then "-" else lst
+	where lst = last as
+
+-- 1行のトークンをキーと値のペアの文字列にして出力
+keyExpand :: Int -> Int -> [ BS.ByteString ] ->  [ BS.ByteString ]
+keyExpand num m ws = [ BS.unwords [ k , v ] | v <- vs ]
+		where k = BS.unwords $ take num ws
+		      vs = mSet m (drop num ws)
+
+parseArgs :: [String] -> (Int,Int)
+parseArgs as = (getNum $ head0 $ filter findNum as, getM $ head1 $ filter findM as)
+	where
+		findNum as = "num=" `isPrefixOf` as
+		findM as = "-" `isPrefixOf` as && length as > 1
+		getNum a = read (drop 4 a)::Int
+		getM a = read (drop 1 a)::Int
+
+		head0 :: [String] -> String
+		head0 [] = "00000"
+		head0 as = head as
+		head1 :: [String] -> String
+		head1 [] = "00001"
+		head1 as = head as
