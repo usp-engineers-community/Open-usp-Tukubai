38d98211307d28087cd1b56a77275e8a450f8051
Ryuichi UEDA <ryuichiueda@gmail.com>
2013-06-09 09:25:34 +0900

    acceleration of delf.hs

--- COMMANDS.HS/delf.hs
+++ COMMANDS.HS/delf.hs
@@ -2,6 +2,7 @@ import System.Environment
 import System.IO
 import Text.ParserCombinators.Parsec
 import Control.Monad
+import Data.ByteString.Lazy.Char8 as BS
 
 {--
 delf（Open usp Tukubai）
@@ -33,20 +34,19 @@ THE SOFTWARE.
 --}
 
 showUsage :: IO ()
-showUsage = do hPutStr stderr
-		("Usage    : delf <f1> <f2> ... <file>\n" ++ 
-		"Sun May 26 21:14:17 JST 2013\n" ++
-		"Open usp Tukubai (LINUX+FREEBSD), Haskell ver.\n")
+showUsage = do System.IO.hPutStr stderr ("Usage    : delf <f1> <f2> ... <file>\n" ++ 
+                "Sun Jun  9 09:23:37 JST 2013\n" ++
+                "Open usp Tukubai (LINUX+FREEBSD), Haskell ver.\n")
 
 main :: IO ()
 main = do
-	args <- getArgs
-	case args of
-		["-h"]     -> showUsage
-		["--help"] -> showUsage
-		_          -> do if f == "-"
-                                     then getContents >>= mainProc fields
-                                     else readFile f >>= mainProc fields
+        args <- getArgs
+        case args of
+                ["-h"]     -> showUsage
+                ["--help"] -> showUsage
+                _          -> do if f == "-"
+                                     then BS.getContents >>= mainProc fields
+                                     else BS.readFile f >>= mainProc fields
                                      where f = getFileName opts
                                            fields = getFields opts
                                            opts = setOpts args
@@ -55,13 +55,13 @@ main = do
 -- output --
 ------------
 
-mainProc :: [(Int,Int)] -> String -> IO ()
-mainProc fs cs = putStr $ unlines [ lineProc fs c | c <- lines cs ]
+mainProc :: [(Int,Int)] -> BS.ByteString -> IO ()
+mainProc fs cs = BS.putStr $ BS.unlines [ lineProc fs c | c <- BS.lines cs ]
 
-lineProc :: [(Int,Int)] -> String -> String
-lineProc fs ln = unwords $ deleteFields nfs zwds
-                 where zwds = zip [1..] (words ln)
-                       nfs = fieldNormalize fs (length zwds)
+lineProc :: [(Int,Int)] -> BS.ByteString -> BS.ByteString
+lineProc fs ln = BS.unwords $ deleteFields nfs zwds
+                 where zwds = Prelude.zip [1..] (BS.words ln)
+                       nfs = fieldNormalize fs (Prelude.length zwds)
 
 fieldNormalize :: [(Int,Int)] -> Int -> [Int]
 fieldNormalize ((a,b):fs) fnum = [c..d] ++ (fieldNormalize fs fnum)
@@ -69,8 +69,8 @@ fieldNormalize ((a,b):fs) fnum = [c..d] ++ (fieldNormalize fs fnum)
                        d = if b > 0 then b else fnum + b
 fieldNormalize [] fnum = []
 
-deleteFields :: [Int] -> [(Int,String)] -> [String]
-deleteFields fs ((n,w):nws) = if n `elem` fs
+deleteFields :: [Int] -> [(Int,BS.ByteString)] -> [BS.ByteString]
+deleteFields fs ((n,w):nws) = if n `Prelude.elem` fs
                               then deleteFields fs nws
                               else w : deleteFields fs nws 
 deleteFields fs [] = []
@@ -84,7 +84,7 @@ data Option = FRange Field Field | FileName String | Error String
 
 getFields :: [Option] -> [(Int,Int)]
 getFields ((FRange a b):opts) = (f a,f b) : getFields opts
-	                        where f (Field c) = c
+                                where f (Field c) = c
 getFields (opt:opts)       = getFields opts
 getFields []               = []
 
@@ -119,7 +119,7 @@ parseNormalField = liftM (Field . read) $ many1 digit
 
 parseNFMinusField :: Parser Field
 parseNFMinusField =  do string "NF-"
-			num <- many1 digit
+                        num <- many1 digit
                         return $ Field ( -1 * (read num) )
 
 parseNFField :: Parser Field
