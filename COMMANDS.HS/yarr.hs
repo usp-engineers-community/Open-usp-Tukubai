7ab491378669d1359ba686212e38e0e756a90e1e
Ryuichi UEDA <ryuichiueda@gmail.com>
2013-07-27 10:03:08 +0900

    cleaning of yarr.hs

--- COMMANDS.HS/yarr.hs
+++ COMMANDS.HS/yarr.hs
@@ -37,7 +37,7 @@ THE SOFTWARE.
 showUsage :: IO ()
 showUsage = do System.IO.hPutStr stderr
 		("Usage    : yarr [num=<n>] [-<m>] <file>\n" ++ 
-		"Fri Jul 26 20:16:22 JST 2013\n" ++
+		"Sat Jul 27 10:02:29 JST 2013\n" ++
 		"Open usp Tukubai (LINUX+FREEBSD), Haskell ver.\n")
 
 main :: IO ()
@@ -46,11 +46,6 @@ main = do args <- getArgs
               ["-h"]     -> showUsage
               ["--help"] -> showUsage
               _          -> do mainProc (setOpts args)
-{--
-              _          -> do readF (getFileName os) >>= mainProc fs
-                                     where fs = getFields os
-                                           os = setOpts args
---}
 
 readF :: String -> IO BS.ByteString
 readF "-" = BS.getContents
@@ -75,7 +70,8 @@ yarr m (a:b:rs) = if compKey a b then yarr' m (x:rs) else (a : yarr m (b:rs))
                   where x = mergeKey a b
 
 yarr' :: Int -> [Record] -> [Record]
-yarr' m ((Record k vs):rs) = if (length vs) > m then Record k (take m vs) : yarr m (Record k (drop m vs): rs) 
+yarr' m ((Record k vs):rs) = if (length vs) > m 
+                             then Record k (take m vs) : yarr m (Record k (drop m vs): rs) 
                              else  (Record k vs) : yarr m rs
 
 toStr :: Record -> BS.ByteString
@@ -95,95 +91,11 @@ myWords :: BS.ByteString -> [BS.ByteString]
 myWords ws = filter (/= BS.pack "") $ split ' ' ws
 
 setOpts :: [String] -> Option
-setOpts as = Option y1 y2 f
-             where x1 = filter ( > 0) [ getNum a | a <- as ]
-                   y1 = if x1 == [] then 0 else head x1
-                   x2 = filter ( > 0) [ getM a | a <- as ]
-                   y2 = if x2 == [] then 0 else head x2
-                   fs = filter (/= "") [ getFileName a | a <- as ]
-                   f = if fs == [] then "-" else head fs
-
-getNum :: String -> Int
-getNum str = if "num=" `DL.isPrefixOf` str
-             then read $ drop 4 str
-             else 0
-        
-getM :: String -> Int
-getM str = if "-" `DL.isPrefixOf` str
-             then read $ drop 1 str
-             else 0
-
-getFileName :: String -> String
-getFileName str = if "-" `DL.isPrefixOf` str then "" else y
-             where y = if "num=" `DL.isPrefixOf` str then "" else str
-                                   
-{--
-------------
--- output --
-------------
-
---mainProc fs cs = BS.putStr $ BS.unlines [ lineProc fs c | c <- BS.lines cs ]
-
-lineProc :: [(Int,Int)] -> BS.ByteString -> BS.ByteString
-lineProc fs ln = BS.unwords $ deleteFields nfs zwds
-                 where zwds = Prelude.zip [1..] (myWords ln)
-                       nfs = fieldNormalize fs (Prelude.length zwds)
-
-fieldNormalize :: [(Int,Int)] -> Int -> [Int]
-fieldNormalize ((a,b):fs) fnum = [c..d] ++ (fieldNormalize fs fnum)
-                 where c = if a > 0 then a else fnum + a
-                       d = if b > 0 then b else fnum + b
-fieldNormalize [] fnum = []
-
-deleteFields :: [Int] -> [(Int,BS.ByteString)] -> [BS.ByteString]
-deleteFields fs ((n,w):nws) = if n `Prelude.elem` fs
-                              then deleteFields fs nws
-                              else w : deleteFields fs nws 
-deleteFields fs [] = []
-
-------------------------
--- parsing of options --
-------------------------
-
-data Field = Field Int
-
-getFields :: [Option] -> [(Int,Int)]
-getFields ((FRange a b):opts) = (f a,f b) : getFields opts
-                                where f (Field c) = c
-getFields (opt:opts)       = getFields opts
-getFields []               = []
-
-getFileName :: [Option] -> String
-getFileName ((FileName s):opts) = s
-getFileName (opt:opts)          = getFileName opts
-getFileName []                  = "-"
-
-parseMonoRange :: Parser Option
-parseMonoRange = do {f <- parseField ; return (FRange f f) }
-
-parseField :: Parser Field
-parseField = try(parseNormalField) <|> try(parseNFMinusField) <|> try(parseNFField)
-
-parseCompRange :: Parser Option
-parseCompRange = do first <- parseField
-                    char '/'
-                    second <- parseField
-                    return ( FRange first second )
-
-parseNormalField :: Parser Field
-parseNormalField = liftM (Field . read) $ many1 digit
-
-parseNFMinusField :: Parser Field
-parseNFMinusField =  do string "NF-"
-                        num <- many1 digit
-                        return $ Field ( -1 * (read num) )
-
-parseNFField :: Parser Field
-parseNFField =  string "NF" >> (return $ Field 0)
-
-parseFileName :: Parser Option
-parseFileName =  many1 ( letter <|> digit <|> symbol ) >>= return . FileName
-
-symbol :: Parser Char
-symbol = oneOf "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
---}
+setOpts as = Option num m f
+             where ns = filter ( DL.isPrefixOf "num=" ) as
+                   num = if ns == [] then 0 else read $ drop 4 $ head ns
+                   ms = filter ( DL.isPrefixOf "-" ) as
+                   m = if ms == [] then 0 else read $ drop 1 $ head ms
+                   str = DL.last as
+                   f = if "num=" `DL.isPrefixOf` str 
+                       then "-" else ( if "-" `DL.isPrefixOf` str then "-" else str )
