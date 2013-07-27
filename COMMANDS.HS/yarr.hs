eb2b9aaf8fa48877c1acfba68aa5777f74217448
Ryuichi UEDA <ryuichiueda@gmail.com>
2013-07-27 09:33:33 +0900

    release of yarr.hs

--- COMMANDS.HS/yarr.hs
+++ COMMANDS.HS/yarr.hs
@@ -1,6 +1,9 @@
-import Data.List
 import System.Environment
 import System.IO
+import Text.ParserCombinators.Parsec
+import Control.Monad
+import Data.ByteString.Lazy.Char8 as BS hiding (filter,drop,head,length,take)
+import Data.List as DL
 
 {--
 yarr（Open usp Tukubai）
@@ -32,97 +35,155 @@ THE SOFTWARE.
 --}
 
 showUsage :: IO ()
-showUsage = do hPutStr stderr
+showUsage = do System.IO.hPutStr stderr
 		("Usage    : yarr [num=<n>] [-<m>] <file>\n" ++ 
-		"Mon Apr 29 16:57:49 JST 2013\n" ++
+		"Fri Jul 26 20:16:22 JST 2013\n" ++
 		"Open usp Tukubai (LINUX+FREEBSD), Haskell ver.\n")
 
 main :: IO ()
-main = do
-	args <- getArgs
-	case args of
-		["-h"]     -> showUsage
-		["--help"] -> showUsage
-		[]         -> do unfoldAll
-		["-"]      -> do unfoldAll
-		_          -> do if (fileName args) == "-" 
-					then getContents >>= mainProc (parseArgs args)
-					else readFile (fileName args) >>= mainProc (parseArgs args)
+main = do args <- getArgs
+          case args of
+              ["-h"]     -> showUsage
+              ["--help"] -> showUsage
+              _          -> do mainProc (setOpts args)
+{--
+              _          -> do readF (getFileName os) >>= mainProc fs
+                                     where fs = getFields os
+                                           os = setOpts args
+--}
 
-splitAtKey :: Int -> [String] -> [ ([String], [String]) ]
-splitAtKey num [] = []
-splitAtKey num (c:cs) = splitAt num (words c) : splitAtKey num cs
+readF :: String -> IO BS.ByteString
+readF "-" = BS.getContents
+readF f = BS.readFile f
 
-mergeByKey :: ([String], [String]) -> [ ([String], [String]) ] -> [ ([String], [String]) ]
-mergeByKey c [] = [c]
-mergeByKey ([""],[""]) (r:rs) = mergeByKey r rs
-mergeByKey c (r:rs) = if (fst c) == (fst r)
-			then mergeByKey (fst c, (snd c)++(snd r)) rs
-			else c : mergeByKey r rs
+data Option = Option Int Int String | Error String deriving Show
 
---単語ごとに全部改行して出力
-unfoldAll = getContents >>= putStrLn . unfold
-	where unfold cs = unwords $ concat [ words c | c <- lines cs ]
+data Record = Record (Maybe BS.ByteString) [BS.ByteString]
 
---yarr実行
-mainProc :: (Int,Int) -> String -> IO()
-mainProc (  0,0) cs = putStrLn . unwords $ concat [ words c | c <- lines cs ]
-mainProc (  0,m) cs = putStr $ mUnfold m cs
---mainProc (num,m) cs = putStr $ keyUnfold num m $ lines cs
-mainProc (num,0) cs = putStr . fold . mergeByKey ([""],[""]) $ splitAtKey num (lines cs)
-mainProc (num,m) cs = putStr . mFold m $ mergeByKey ([""],[""]) $ splitAtKey num (lines cs)
+mainProc :: Option -> IO ()
+mainProc (Option num m file) = readF file >>= mainProc' num m
 
--- -<m> オプションのときに1行をm単語ずつ折り返し
-fold :: [ ([String], [String]) ] -> String
-fold cs = unlines [ fold' (fst c) (snd c) | c <- cs ]
-		where fold' k vs = (unwords k) ++ " " ++ (unwords vs) 
+mainProc' :: Int -> Int -> BS.ByteString -> IO ()
+mainProc' num m str = BS.putStr $ BS.unlines $ DL.map toStr $ yarr m rs
+                      where rs = [ makeRecord num ln | ln <- BS.lines str ]
 
--- -<m> オプションのときに1行をm単語ずつ折り返し
-mFold :: Int -> [ ([String], [String]) ] -> String
-mFold m cs = unlines $ concat [ mFold' m (fst c) (snd c) | c <- cs ]
+yarr :: Int -> [Record] -> [Record]
+yarr m []       = []
+yarr m (a:[])   = [a]
+yarr 0 (a:b:rs) = if compKey a b then yarr 0 ((mergeKey a b) : rs) else (a : yarr 0 (b:rs))
+yarr m (a:b:rs) = if compKey a b then yarr' m (x:rs) else (a : yarr m (b:rs))
+                  where x = mergeKey a b
 
-mFold' :: Int -> [String] -> [String] -> [String]
-mFold' m k vs = [(unwords k) ++ " " ++ c | c <- mSet m vs ]
+yarr' :: Int -> [Record] -> [Record]
+yarr' m ((Record k vs):rs) = if (length vs) > m then Record k (take m vs) : yarr m (Record k (drop m vs): rs) 
+                             else  (Record k vs) : yarr m rs
 
--- -<m> オプションのときに1行をm単語ずつ折り返し
-mUnfold :: Int -> String -> String
---mUnfold m cs = unlines $ concat [ mSet m ( words c ) | c <- lines cs ]
-mUnfold m cs = unlines $ mSet m ( words c )
-	where c = unwords $ lines cs
+toStr :: Record -> BS.ByteString
+toStr (Record k vs) = if k == Nothing then BS.unwords vs else BS.unwords ((f k):vs)
+                      where f (Just x) = x
 
--- 単語のリストから単語をm個ずつまとめたリストへ
-mSet :: Int -> [ String ] -> [ String ]
-mSet m [] = []
-mSet m ws = (unwords $ fst s) : mSet m (snd s)
-		where s = splitAt m ws 
+compKey (Record k vs) (Record k2 vs2 )  = k == k2
+mergeKey (Record k vs) (Record k2 vs2 ) = Record k (vs ++ vs2)  
 
--- キー(num=<n>)への対応
-keyUnfold :: Int -> Int -> [ String ] -> String
-keyUnfold num m cs = unlines $ concat [ keyExpand num m ( words c ) | c <- cs ]
+makeRecord :: Int -> BS.ByteString -> Record
+makeRecord 0   str = Record Nothing (myWords str)
+makeRecord num str = Record k (drop num vs)
+                     where vs = myWords str
+                           k  = Just (BS.unwords $ take num vs)
 
--- オプションからファイル名取り出し
-fileName :: [String] -> String
-fileName as = if "num=" `isPrefixOf` lst || "-" `isPrefixOf` lst then "-" else lst
-	where lst = last as
+myWords :: BS.ByteString -> [BS.ByteString]
+myWords ws = filter (/= BS.pack "") $ split ' ' ws
 
--- 1行のトークンをキーと値のペアの文字列にして出力
-keyExpand :: Int -> Int -> [ String ] ->  [ String ]
-keyExpand num m ws = [ unwords [ k , v ] | v <- vs ]
-		where k = unwords $ take num ws
-		      vs = mSet m (drop num ws)
+setOpts :: [String] -> Option
+setOpts as = Option y1 y2 f
+             where x1 = filter ( > 0) [ getNum a | a <- as ]
+                   y1 = if x1 == [] then 0 else head x1
+                   x2 = filter ( > 0) [ getM a | a <- as ]
+                   y2 = if x2 == [] then 0 else head x2
+                   fs = filter (/= "") [ getFileName a | a <- as ]
+                   f = if fs == [] then "-" else head fs
 
--- オプションからパラメータ取り出し
-parseArgs :: [String] -> (Int,Int)
-parseArgs as = (getNum $ head0 $ filter findNum as, getM $ head1 $ filter findM as)
-	where
-		findNum a = "num=" `isPrefixOf` a
-		getNum a = read (drop 4 a)::Int
-		head0 :: [String] -> String
-		head0 [] = "num=0"
-		head0 as = head as
+getNum :: String -> Int
+getNum str = if "num=" `DL.isPrefixOf` str
+             then read $ drop 4 str
+             else 0
+        
+getM :: String -> Int
+getM str = if "-" `DL.isPrefixOf` str
+             then read $ drop 1 str
+             else 0
 
-		findM a = "-" `isPrefixOf` a && length a > 1
-		getM a = read (drop 1 a)::Int
-		head1 :: [String] -> String
-		head1 [] = "-0"
-		head1 as = head as
+getFileName :: String -> String
+getFileName str = if "-" `DL.isPrefixOf` str then "" else y
+             where y = if "num=" `DL.isPrefixOf` str then "" else str
+                                   
+{--
+------------
+-- output --
+------------
+
+--mainProc fs cs = BS.putStr $ BS.unlines [ lineProc fs c | c <- BS.lines cs ]
+
+lineProc :: [(Int,Int)] -> BS.ByteString -> BS.ByteString
+lineProc fs ln = BS.unwords $ deleteFields nfs zwds
+                 where zwds = Prelude.zip [1..] (myWords ln)
+                       nfs = fieldNormalize fs (Prelude.length zwds)
+
+fieldNormalize :: [(Int,Int)] -> Int -> [Int]
+fieldNormalize ((a,b):fs) fnum = [c..d] ++ (fieldNormalize fs fnum)
+                 where c = if a > 0 then a else fnum + a
+                       d = if b > 0 then b else fnum + b
+fieldNormalize [] fnum = []
+
+deleteFields :: [Int] -> [(Int,BS.ByteString)] -> [BS.ByteString]
+deleteFields fs ((n,w):nws) = if n `Prelude.elem` fs
+                              then deleteFields fs nws
+                              else w : deleteFields fs nws 
+deleteFields fs [] = []
+
+------------------------
+-- parsing of options --
+------------------------
+
+data Field = Field Int
+
+getFields :: [Option] -> [(Int,Int)]
+getFields ((FRange a b):opts) = (f a,f b) : getFields opts
+                                where f (Field c) = c
+getFields (opt:opts)       = getFields opts
+getFields []               = []
+
+getFileName :: [Option] -> String
+getFileName ((FileName s):opts) = s
+getFileName (opt:opts)          = getFileName opts
+getFileName []                  = "-"
+
+parseMonoRange :: Parser Option
+parseMonoRange = do {f <- parseField ; return (FRange f f) }
+
+parseField :: Parser Field
+parseField = try(parseNormalField) <|> try(parseNFMinusField) <|> try(parseNFField)
+
+parseCompRange :: Parser Option
+parseCompRange = do first <- parseField
+                    char '/'
+                    second <- parseField
+                    return ( FRange first second )
+
+parseNormalField :: Parser Field
+parseNormalField = liftM (Field . read) $ many1 digit
+
+parseNFMinusField :: Parser Field
+parseNFMinusField =  do string "NF-"
+                        num <- many1 digit
+                        return $ Field ( -1 * (read num) )
+
+parseNFField :: Parser Field
+parseNFField =  string "NF" >> (return $ Field 0)
+
+parseFileName :: Parser Option
+parseFileName =  many1 ( letter <|> digit <|> symbol ) >>= return . FileName
+
+symbol :: Parser Char
+symbol = oneOf "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+--}
