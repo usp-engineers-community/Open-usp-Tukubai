bc7490672f88000a0c890bafbe586087b3dc1351
Ryuichi UEDA <ryuichiueda@gmail.com>
2013-08-19 15:05:11 +0900

    release of kasan.hs

--- COMMANDS.HS/kasan.hs
+++ COMMANDS.HS/kasan.hs
@@ -36,7 +36,7 @@ THE SOFTWARE.
 
 showUsage :: IO ()
 showUsage = do System.IO.hPutStr stderr ("Usage    : kasan [+r] [ref=<ref>] key=<n> <file>\n" ++ 
-                "Mon Aug 19 11:03:28 JST 2013\n" ++
+                "Mon Aug 19 15:05:05 JST 2013\n" ++
                 "Open usp Tukubai (LINUX+FREEBSD), Haskell ver.\n")
 
 main :: IO ()
@@ -89,11 +89,15 @@ newRecord (Record ws refstr fs vs) (Record pws prefstr pfs pvs)
 
 parseLine :: Int -> [Int] -> BS.ByteString -> Record
 parseLine ref ks ln = Record ws refstr ks vs
-          where ws = BS.words ln
+          where ws = myWords ln
                 refstr = if ref == 0 then (BS.pack "") else (ws !! (ref-1))
                 vs = f ks ws
                 f [] _      = []
                 f (k:ks) ws = (read (BS.unpack $ ws !! (k-1))::Double ) : f ks ws
+
+myWords :: BS.ByteString -> [BS.ByteString]
+myWords line = filter (/= x) $ BS.split ' ' line
+               where x = BS.pack ""
 
 data Record = Record [BS.ByteString] BS.ByteString [Int] [Double] deriving Show
 
