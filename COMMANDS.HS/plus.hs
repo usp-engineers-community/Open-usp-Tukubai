c257b300bd04415079873f5f4b62443f946ad041
Ryuichi UEDA <ryuichiueda@gmail.com>
2015-04-26 13:22:37 +0900

    Add plus.hs

---
+++ COMMANDS.HS/plus.hs
@@ -0,0 +1,30 @@
+import System.Environment
+import System.Exit
+import System.IO
+import Data.Fixed
+
+showUsage :: IO ()
+showUsage = hPutStr stderr (
+                "Usage    : plus <n1> <n2> ... \n" ++
+                "Sun Apr 26 11:08:56 JST 2015\n" ++
+                "Open usp Tukubai (LINUX+FREEBSD+Mac), Haskell ver.\n")
+             >> exitWith (ExitFailure 1)
+
+die str = hPutStr stderr ( "Error[map] : " ++ str ++ "\n")
+          >> exitWith (ExitFailure 1)
+
+main = do args <- getArgs
+          case args of
+              ["-h"]     -> showUsage
+              []         -> showUsage
+              nums       -> main' nums
+
+data E100 = E100
+instance HasResolution E100 where resolution _ = 10 ^ 100
+
+main' :: [String] -> IO ()
+main' strs = putStrLn . shaveZero . show . sum $ map (\n -> read n :: (Fixed E100)) strs
+
+shaveZero :: String -> String
+shaveZero str = reverse $ dropWhile (== '.') $ dropWhile (== '0') $ reverse str
+
