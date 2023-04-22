8a010852856d74106e4af0e7bf95ca44ac032812
Hinata Yanagi <h-yanagi@usp-lab.com>
2023-04-22 19:19:51 +0900

    Implement numchar.hs.

---
+++ COMMANDS.HS/numchar.hs
@@ -0,0 +1,58 @@
+#!/usr/bin/env runghc --
+import Data.Char (ord)
+import Numeric (showHex)
+import System.Environment
+import System.IO
+
+{--
+numchar（Open usp Tukubai）
+
+designed by Nobuaki Tounaka
+written  by Hinata Yanagi
+
+The MIT License
+
+Copyright (C) 2023 Universal Shell Programming Laboratory
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+--}
+
+showUsage :: IO ()
+showUsage = do
+        System.IO.hPutStr stderr "Usage    : numchar\n"
+        System.IO.hPutStr stderr "Version  : Sat Apr 22 19:19:33 JST 2023\n"
+        System.IO.hPutStr stderr "Open usp Tukubai (LINUX+FREEBSD)\n"
+
+main :: IO ()
+main = do
+    args <- getArgs
+    case args of
+                []            -> numchar
+                ["-h"]        -> showUsage
+                ["--help"]    -> showUsage
+                ["--version"] -> showUsage
+                _             -> showUsage
+
+numchar = do
+  eof <- hIsEOF stdin
+
+  if eof then return () else do
+    character <- hGetChar stdin
+    System.IO.putStr $ "&#x" ++ showHex (ord character) "" ++ ";"
+    numchar
